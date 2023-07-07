42a0511c61fb6a3241b8fac9c0144cfebf2367d4
Bar Cohen <sscohen99@gmail.com>
2023-07-07 20:28:06 +0300

    profile component refactored

--- src/comps/navBar/Profile.jsx
+++ src/comps/navBar/Profile.jsx
@@ -1,4 +1,3 @@
-import {useState} from "react"
 import "./profile.css"
 import Details from "./Details"
 import {Link, Route, Routes} from "react-router-dom"
@@ -6,67 +5,32 @@ import AllCategories from "./AllCategories"
 import {FiHome} from "react-icons/fi"
 import ChangeCategories from './ChangeCategories'
 
-export default function Profile() {
-    const [over, setOver] = useState(false)
-    const [over1, setOver1] = useState(false)
-    const [over2, setOver2] = useState(false)
+const Profile = () => (
+    <div className="main_profile">
+        <div className="div_btn_profile">
+            <Link className="btn_profile" to="/profile/changeCategories">
+                Change categories
+            </Link>
+            <Link className="btn_profile" to="/profile/details">
+                my details
+            </Link>
 
-    function changeDetails() {
-        if (over1 || over2) {
-            setOver(false)
-            setOver2(false)
-            setOver1(true)
-        } else {
-            setOver1(true)
-            setOver(false)
-            setOver2(false)
-        }
-    }
-
-    function changeAllCategories() {
-        if (over || over1) {
-            setOver(false)
-            setOver1(false)
-            setOver2(true)
-        } else {
-            setOver2(true)
-            setOver1(false)
-            setOver(false)
-        }
-    }
-
-
-    return (
-        <div className="main_profile">
-            <div className="div_btn_profile">
-                <Link className="btn_profile" to="/changeCategories">
-                    Change categories
-                </Link>
-                <button className="btn_profile" onClick={changeDetails}>
-                    my details
-                </button>
-
-                <button className="btn_profile" onClick={changeAllCategories}>
-                    all categories
-                </button>
-            </div>
-            <div className="list_catgeris">
-                <Routes>
-                    <Route index element={<Profile/>}>
-                        <Route path="changeCategories" element={<ChangeCategories/>}/>
-                        <Route path="details" element={<Details/>}/>
-                        <Route path="allCategories" element={<AllCategories/>}/>
-                    </Route>
-                </Routes>
-            </div>
-
-
-            <div>
-                <Link className="back_home" to={"/"}>
-                    <FiHome/> home
-                </Link>
-
-            </div>
+            <Link className="btn_profile" to="/profile/allCategories">
+                all categories
+            </Link>
         </div>
-    )
-}
+
+        <div className="list_catgeris">
+            <Routes>
+                <Route path="changeCategories" element={<ChangeCategories/>}/>
+                <Route path="details" element={<Details/>}/>
+                <Route path="allCategories" element={<AllCategories/>}/>
+            </Routes>
+        </div>
+
+        <Link className="home-button" to={"/"}>
+            <FiHome/> home
+        </Link>
+    </div>
+)
+export default Profile
