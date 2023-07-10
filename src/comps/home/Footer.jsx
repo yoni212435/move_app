61ffae72fb0d6e954e8c8065b4210fb39ed85d69
Bar Cohen <sscohen99@gmail.com>
2023-07-10 19:15:49 +0300

    fixed home button position

--- src/comps/home/Footer.jsx
+++ src/comps/home/Footer.jsx
@@ -3,17 +3,19 @@ import {FiHome} from 'react-icons/fi'
 
 const Footer = () => (
     <div className="footer-container">
-        <div className="footer-right">
-            {window.location.pathname !== '/' &&
-                <Link to={'/'} className="home-button">
-                    <FiHome/> Home
-                </Link>}
-        </div>
+        <div className="footer-left"/>
 
         <div className="footer-center">
             <span>פרטים ליצירת קשר</span>
             <br/>
             <span>cinema.co.il | | | 052-3060345</span>
+        </div>
+
+        <div className="footer-right">
+            {window.location.pathname !== '/' &&
+                <Link to={'/'} className="home-button">
+                    <FiHome viewBox={"0 0 24 30"}/> <span>Home</span>
+                </Link>}
         </div>
     </div>
 )
