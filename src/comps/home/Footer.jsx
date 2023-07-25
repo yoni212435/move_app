7ef9edf989d78f31d0930cf8e9a7164197ebbc65
Bar Cohen <sscohen99@gmail.com>
2023-07-25 12:16:02 +0300

    footer contact details fix

--- src/comps/home/Footer.jsx
+++ src/comps/home/Footer.jsx
@@ -9,13 +9,15 @@ const Footer = () => {
             <div className="footer-left"/>
 
             <div className="footer-center">
-                <span>פרטים ליצירת קשר</span>
+                <span>{'פרטים ליצירת קשר'}</span>
                 <br/>
-                <span>cinema.co.il  | | |
-                <a href={"tel:052-3060345"} style={{textDecoration: "none", color: "rgb(194, 255, 255)"}}>
-                    052-3060345
-                </a>
-            </span>
+                <span>{'cinema.co.il'}</span>
+                <span>{' | '}</span>
+                <span>
+                    <a href={"tel:052-3060345"} style={{textDecoration: "none", color: "rgb(194, 255, 255)"}}>
+                        {'052-3060345'}
+                    </a>
+                </span>
             </div>
 
             <div className="footer-right">
